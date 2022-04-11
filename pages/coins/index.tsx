77915323da81f9cd93dd4b508938ac5da2e5ba2e
Robert Na <robert@teamo2.kr>
2022-04-11 12:12:31 +0900

    Incremental Static Regeneration

--- pages/coins/index.tsx
+++ pages/coins/index.tsx
@@ -1,10 +1,10 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import Coin from "../../components/Coin";
 import Counter from "../../components/Counter";
 import Layout from "../../components/Layout";
 import { CounterProps } from "../_app";
 
-export default function Coins({ coins, counter, dispatchCounter }: InferGetServerSidePropsType<typeof getServerSideProps> & CounterProps) {
+export default function Coins({ coins, counter, dispatchCounter }: InferGetStaticPropsType<typeof getStaticProps> & CounterProps) {
   return (
     <Layout title="Coin">
       <h1>Coin</h1>
@@ -26,13 +26,18 @@ export default function Coins({ coins, counter, dispatchCounter }: InferGetServe
   );
 }
 
-export const getServerSideProps: GetServerSideProps<ServerSideProps> = async (context) => {
+export const getStaticProps: GetStaticProps<ServerSideProps> = async (context) => {
   const coins = await (await fetch('https://api.upbit.com/v1/market/all')).json();
+
+  if (!coins || !coins.length) {
+    throw new Error('Fail to fetch');
+  }
 
   return {
     props: {
       coins,
     },
+    revalidate: 60 * 60,
   };
 };
 
